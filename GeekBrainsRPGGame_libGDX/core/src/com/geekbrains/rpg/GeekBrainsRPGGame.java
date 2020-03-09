1dc3bcd3301bb0681aa8f7cdcb06672adc4eaac6
KonstantinMalkov <mkv.kasatin@gmail.com>
2020-03-09 19:40:27 +0500

    change1

--- GeekBrainsRPGGame_libGDX/core/src/com/geekbrains/rpg/GeekBrainsRPGGame.java
+++ GeekBrainsRPGGame_libGDX/core/src/com/geekbrains/rpg/GeekBrainsRPGGame.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
 public class GeekBrainsRPGGame extends ApplicationAdapter {
 	private SpriteBatch batch;
